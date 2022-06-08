a1e283e69ac6436732b0064a48c5d68a8cce81e6
pragya265 <singhalpragya265@gmail.com>
2022-06-08 22:37:29 +0000

    changing data

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ varlist=['TotalPop', 'Men', 'Women', 'Hispanic',
        'WorkAtHome', 'MeanCommute', 'Employed', 'PrivateWork', 'PublicWork',
        'SelfEmployed', 'FamilyWork', 'Unemployment', 'RUCC_2013']
 
-df=pd.read_csv('resources/new_york.csv')
+df=pd.read_pickle('resources/va-stats.pkl')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
